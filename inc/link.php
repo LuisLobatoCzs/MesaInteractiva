7046cb94b0c158c1914f7eff9060acd60f0af665
LuisLobatoCzs <luis.lobatoczs@outlook.com>
2019-02-19 02:17:26 -0600

    Actualización del index

--- inc/link.php
+++ inc/link.php
@@ -9,5 +9,4 @@
 <script src="js/jquery.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
 <script src="js/autohidingnavbar.min.js"></script>
-<script src="js/main.js"></script>
 <script src="js/menuDesplegable.js"></script>
